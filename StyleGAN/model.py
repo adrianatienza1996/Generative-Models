b4c8c61b0759fe4c47f6e4e56173081ffc480ee0
Adrian <adrian.atienza.1996@gmail.com>
2021-04-03 13:53:38 +0200

    gitignore modified

--- StyleGAN/model.py
+++ StyleGAN/model.py
@@ -89,7 +89,7 @@ class GeneratorBlock(nn.Module):
         if self.pad_channels:
             self.channel_padding = ChannelPadding()
         if self.use_upsaple:
-            self.upsample = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
+            self.upsample = nn.Upsample(scale_factor=2, mode="nearest")
         self.miniblock1 = GeneratorMiniBlock(in_chan=c_in, out_chan=c_out)
         self.miniblock2 = GeneratorMiniBlock(in_chan=c_out, out_chan=c_out)
 
@@ -108,7 +108,6 @@ class Generator(nn.Module):
         self.mapping_noise = MappingNoise()
         self.block1 = GeneratorBlock(use_upsample=False)
         self.block2 = GeneratorBlock()
-        #self.block3 = GeneratorBlock()
         self.block3 = GeneratorBlock(c_in=512, c_out=256)
         self.block4 = GeneratorBlock(c_in=256, c_out=128)
         self.block5 = GeneratorBlock(c_in=128, c_out=64)
@@ -178,38 +177,29 @@ class Discriminator(nn.Module):
             nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1),
             nn.LeakyReLU(0.2),
             nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1))            # 128, 128
-
+        self.upsampler0 = Upsample_Channels(scale_factor=(16 / 3, 1, 1))
         self.upsampler1 = Upsample_Channels(scale_factor=(32//16, 1, 1))
         self.maxpool1 = nn.MaxPool2d(kernel_size=2)
-        self.block2 = Discriminator_Block(32, 32)                             # 64 x 64
-        self.block3 = Discriminator_Block(32, 64)                             # 32 x 32
-        self.block4 = Discriminator_Block(64, 128)                            # 16 x 16
-        self.block5 = Discriminator_Block(128, 256)                            # 8 x 8
-        #self.block6 = Discriminator_Block(256, 512)                           # 4 x 4
-        self.block6 = nn.Sequential(
-            nn.Conv2d(256, 256, kernel_size=4, stride=1, padding=0),
-            nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.2))
+        self.block2 = Discriminator_Block(32, 64)                             # 64 x 64
+        self.block3 = Discriminator_Block(64, 128)                            # 32 x 32
+        self.block4 = Discriminator_Block(128, 256)                           # 16 x 16
+        self.block5 = Discriminator_Block(256, 512)                           # 8 x 8
         self.maxpool7 = nn.MaxPool2d(kernel_size=4)
-        self.classifier = nn.Linear(256, 1)
+        self.classifier = nn.Linear(512, 1)
 
     def set_alpha(self, index, value):
             self.alpha_values[index] = min(value,  self.alpha_values[index])
 
 
     def forward (self, x):
-        h = self.initial_block(x)
-        h1 = self.upsampler1(h)
-        h2 = self.block1(h)
-        h = self.alpha_values[0] * h1 + (1 - self.alpha_values[0]) * h2
+        h = self.alpha_values[5] * self.upsampler0(x) + (1 - self.alpha_values[5]) * self.initial_block(x)
+        h = self.alpha_values[4] * self.upsampler1(h) + (1 - self.alpha_values[4]) * self.block1(h)
         h = self.maxpool1(h)
-        h = self.block2(h, self.alpha_values[1])
+        h = self.block2(h, self.alpha_values[3])
         h = self.block3(h, self.alpha_values[2])
-        h = self.block4(h, self.alpha_values[3])
-        h = self.block5(h, self.alpha_values[4])
-        h1 = self.block6(h)
-        h2 = self.maxpool7(h)
-        h = h2 * self.alpha_values[5] + h1 * (1 - self.alpha_values[5])
+        h = self.block4(h, self.alpha_values[1])
+        h = self.block5(h, self.alpha_values[0])
+        h = self.maxpool7(h)
         return self.classifier(h.squeeze())
 
 
--- StyleGAN/train_procedure.py
+++ StyleGAN/train_procedure.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torchvision
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+print(device)
 from torchsummary import summary
 from torch.utils.tensorboard import SummaryWriter
 from utils import *
@@ -20,9 +21,9 @@ dis = Discriminator().to(device)
 summary(dis, (3, 128, 128), device=device)
 
 # initializate optimizer
-lr = 0.0002
-beta_1 = 0.5
-beta_2 = 0.999
+lr = 0.0015
+beta_1 = 0.0
+beta_2 = 0.99
 
 opt_gen = torch.optim.Adam(gen.parameters(), lr=lr, betas=(beta_1, beta_2))
 opt_critic = torch.optim.Adam(dis.parameters(), lr=lr, betas=(beta_1, beta_2))
@@ -34,8 +35,7 @@ transforms = transforms.Compose([
         mean=[0.485, 0.456, 0.406],
         std=[0.229, 0.224, 0.225])])
 
-batch_size = 16
-
+batch_size = 10
 dataset = datasets.ImageFolder(root="F:/FFHQ", transform=transforms)
 loader = DataLoader(
     dataset,
@@ -44,31 +44,39 @@ loader = DataLoader(
     )
 
 # for tensorboard plotting
-
 fixed_noise = torch.randn(batch_size, 512).to(device)
 fixed_input = torch.zeros(batch_size, 512, 4, 4).to(device)
 writer_real = SummaryWriter(f"logs/GAN/real")
 writer_fake = SummaryWriter(f"logs/GAN/fake")
 step = 0
-epoch_upsizing_model = np.array([0, 15, 30, 45, 60, 75])
 
-for epoch in range(100):
+epoch_upsizing_model = np.array([0, 10, 20, 35, 55, 75])
+for epoch in range(110):
     print("Epoch: " + str(epoch))
     print("Generator alpha values: " + str(gen.alpha_values))
     print("Discriminator alpha values: " + str(dis.alpha_values))
 
-    alpha_values = np.array([0.75, 0.5, 0.25, 0], dtype=np.float32)
-    idx_set_alpha = np.int32(np.quantile(np.arange(len(loader) * 15), [0, 0.25, 0.5, 0.75]))
-
+    flag = 1
+    alpha_values = np.array([0.90, 0.80, 0.70, 0.60, 0.50, 0.40, 0.30, 0.20, 0.10, 0.0], dtype=np.float32)
     for batch_idx, (x, _) in enumerate(tqdm(iter(loader))):
 
-        if epoch in epoch_upsizing_model:
+        if epoch in epoch_upsizing_model and flag:
             level_to_update = np.argmax(epoch_upsizing_model == epoch)
             counter_iter = 0
+            flag = 0
 
-            if counter_iter in idx_set_alpha:
-                gen.set_alpha(level_to_update, alpha_values[batch_idx == idx_set_alpha][0])
-                dis.set_alpha(level_to_update, alpha_values[batch_idx == idx_set_alpha][0])
+            try:
+                num_epochs_level = epoch_upsizing_model[level_to_update + 1] - epoch_upsizing_model[level_to_update]
+            except IndexError:
+                num_epochs_level = 20
+
+            idx_set_alpha = np.array(np.quantile(np.arange(len(loader) * num_epochs_level),
+                                     [0, 0.10, 0.20, 0.30, 0.40, 0.5, 0.60, 0.70, 0.80, 0.90]),
+                                     dtype=np.int32)
+
+        if counter_iter in idx_set_alpha:
+            gen.set_alpha(level_to_update, alpha_values[counter_iter == idx_set_alpha][0])
+            dis.set_alpha(level_to_update, alpha_values[counter_iter == idx_set_alpha][0])
 
         x = x.to(device)
         cur_batch_size = x.shape[0]
@@ -77,6 +85,7 @@ for epoch in range(100):
         dis.zero_grad()
         z = torch.randn((cur_batch_size, 512), device=device)
         fake = gen(fixed_input, z)
+
         critic_real = dis(x).reshape(-1)
         critic_fake = dis(fake).reshape(-1)
         gp = gradient_penalty(dis, x, fake, device=device)
@@ -87,6 +96,12 @@ for epoch in range(100):
         loss_critic.backward(retain_graph=True)
         opt_critic.step()
 
+        """
+        loss_critic = get_crit_loss(fake, x, dis, loss_fn)
+        loss_critic.backward(retain_graph=True)
+        opt_critic.step()
+        """
+
         # Training Generator
         gen.zero_grad()
         z2 = torch.randn((cur_batch_size, 512), device=device)
@@ -95,11 +110,10 @@ for epoch in range(100):
         loss_gen = -torch.mean(gen_fake)
         loss_gen.backward()
         opt_gen.step()
-
         counter_iter += 1
 
         # Print losses occasionally and print to tensorboard
-        if batch_idx % 100 == 0 and batch_idx > 0:
+        if batch_idx % 500 == 0 and batch_idx > 0:
             with torch.no_grad():
                 fake = gen(fixed_input, fixed_noise)
 
@@ -114,6 +128,8 @@ for epoch in range(100):
                 )
             step += 1
 
-torch.save(gen.to("cpu").state_dict(), "Saved_Model/my_gen.pth")
-torch.save(dis.to("cpu").state_dict(), "Saved_Model/my_dis.pth")
-print("Model Saved")
+    torch.save(gen.to("cpu").state_dict(), "Saved_Model/my_gen.pth")
+    torch.save(dis.to("cpu").state_dict(), "Saved_Model/my_dis.pth")
+    gen.to(device)
+    dis.to(device)
+    print("Model Saved")
